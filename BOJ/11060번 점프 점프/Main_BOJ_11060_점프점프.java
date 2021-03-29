be1e6937524ed6c1c77d88a0386ad6700ea72e4d
JangHW <ds6tvi@gmail.com>
2021-03-29 19:21:01 +0900

    11060번 점프 점프

---
+++ BOJ/11060번 점프 점프/Main_BOJ_11060_점프점프.java
@@ -0,0 +1,49 @@
+package com.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+5
+0 0 0 1 0
+
+10
+1 2 0 1 3 2 1 5 4 2
+ */
+public class Main_BOJ_11060_점프점프 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[] jump = new int[N]; // 점프 미로
+
+		for (int i = 0; i < N; i++)
+			jump[i] = Integer.parseInt(st.nextToken());
+
+		int[] dp = new int[N]; // index번까지 가는데 필요한 점프 수 (최적해)
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[0] = 0;
+
+		for (int i = 0; i < N; i++) {
+			int cnt = jump[i];
+			for (int j = 1; j <= cnt; j++) {
+				if (dp[i] == Integer.MAX_VALUE && i != 0)
+					continue;
+				if (i + j < N)
+					dp[i + j] = Math.min(dp[i] + 1, dp[i + j]);
+			}
+		}
+
+		if (dp[N - 1] != Integer.MAX_VALUE)
+			System.out.println(dp[N - 1]);
+		else
+			System.out.println(-1);
+
+		br.close();
+	}
+
+}
