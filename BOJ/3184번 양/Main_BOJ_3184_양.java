2cf205402b4b379cf6960278fa8912da91e9e725
JangHW <ds6tvi@gmail.com>
2021-06-19 21:01:44 +0900

    3184번 양

---
+++ BOJ/3184번 양/Main_BOJ_3184_양.java
@@ -0,0 +1,89 @@
+package com.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_3184_양 {
+
+	static int R, C; // 행, 열
+	static char[][] map;
+	static boolean[][] visited;
+	static int shipCnt, woolfCnt;
+
+	private static void BFS(int x, int y) {
+		int[] dx = { 0, 0, -1, 1 };
+		int[] dy = { -1, 1, 0, 0 };
+
+		Queue<int[]> q = new LinkedList<int[]>();
+		q.add(new int[] { x, y });
+		visited[y][x] = true;
+
+		int cnt1 = 0, cnt2 = 0;
+		if (map[y][x] == 'o')
+			cnt1++;
+		else if (map[y][x] == 'v')
+			cnt2++;
+
+		while (!q.isEmpty()) {
+			int[] top = q.poll();
+			int sX = top[0];
+			int sY = top[1];
+			for (int dir = 0; dir < 4; dir++) {
+				int nX = sX + dx[dir];
+				int nY = sY + dy[dir];
+				if (nX >= 1 && nX <= C && nY >= 1 && nY <= R) {
+					if (map[nY][nX] != '#' && !visited[nY][nX]) {
+						visited[nY][nX] = true;
+						if (map[nY][nX] == 'o')
+							cnt1++;
+						else if (map[nY][nX] == 'v')
+							cnt2++;
+						q.add(new int[] { nX, nY });
+					}
+				}
+			}
+
+		}
+
+		if (cnt1 > cnt2) {
+			shipCnt += cnt1;
+		} else {
+			woolfCnt += cnt2;
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+
+		map = new char[R + 1][C + 1];
+		visited = new boolean[R + 1][C + 1];
+
+		for (int y = 1; y <= R; y++) {
+			String input = br.readLine();
+			for (int x = 1; x <= C; x++) {
+				map[y][x] = input.charAt(x - 1);
+			}
+		}
+
+		for (int y = 1; y <= R; y++) {
+			for (int x = 1; x <= C; x++) {
+				if (!visited[y][x] && map[y][x] != '#') {
+					BFS(x, y);
+				}
+			}
+		}
+
+		System.out.println(shipCnt + " " + woolfCnt);
+
+		br.close();
+	}
+
+}
