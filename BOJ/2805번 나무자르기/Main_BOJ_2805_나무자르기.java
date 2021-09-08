688915111587e192cca7f64ad44ee1e85e702ca1
JangHW <ds6tvi@gmail.com>
2021-09-08 22:52:40 +0900

    2805번 나무 자르기

--- BOJ/2805번 나무자르기/Main_BOJ_2805_나무자르기.java
+++ BOJ/2805번 나무자르기/Main_BOJ_2805_나무자르기.java
@@ -1,46 +1,59 @@
+
+/*
+ * N개의 나무를 일렬로 세우고 H길이의 절단기로 자른다.
+ * H길이로 자르고 남은 나무들의 길이 합을 M으로 만들어야함
+ * 파라메트릭 서치로 빠르게 찾자 <- 최대값 최소값 찾는 최적화 문제를 결정 문제로 바꾸는 것
+ * 제발 데이터 범위 어제도 틀렸잖아 제발
+ */
+
 package com.baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_BOJ_2805_나무자르기 {
+
+	static int N, M; // 나무의 수, 가져가야하는 나무의 총 길이
+	static long[] trees; // 나무의 길이
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		int N = Integer.parseInt(st.nextToken());
-		long M = Integer.parseInt(st.nextToken());
-		long[] trees = new long[N];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
 		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
+
+		long left = 0, right = 0;
+		trees = new long[N + 1];
+		for (int i = 1; i <= N; i++) {
 			trees[i] = Integer.parseInt(st.nextToken());
+			if (trees[i] > right)
+				right = trees[i]; // 가장 긴 나무의 길이
 		}
 
-		Arrays.sort(trees);
-
-		long left = 0, right = trees[N - 1], result = 0;
-
+		long answer = 0;
 		while (left <= right) {
-			long mid = (left + right) / 2;
-			long sum = 0;
-			for (int i = 0; i < N; i++) {
-				if (trees[i] > mid)
-					sum += (trees[i] - mid);
+			long result = 0;
+			long mid = (left + right) / 2; // 절단기의 길이 설정
+
+			for (int i = 1; i <= N; i++) {
+				long cutted = (trees[i] - mid) < 0 ? 0 : trees[i] - mid;
+				result += cutted;
+			}
+			
+			if (result < M) { // 자른 결과물이 생각보다 적은 경우
+				right = mid - 1;
+			} else if (M <= result) { // 자른 결과물이 생각보다 많거나 같은 경우
+				answer = mid;
+				left = mid + 1;
 			}
 
-			if (sum >= M) { // 실제 자른 나무들이 잘라야할 나무보다 많다면
-				left = mid + 1;
-				result = mid;
-			} else if (sum < M) {
-				right = mid - 1;
-			}
 		}
-		
-		System.out.println(result);
+
+		System.out.println(answer);
 
 		br.close();
 	}
